5488b0f7045c03e65ad0028910ea3fb8cbad0d61
long2ice <long2ice@prismslight.com>
2020-03-25 16:31:16 +0800

    tables support

--- mygf/decorators.py
+++ mygf/decorators.py
@@ -3,18 +3,19 @@ import functools
 from mygf import SyncCache
 
 
-def register_sync(table, schema=None):
+def register_sync(*tables, schema=None):
     """
     register cache sync
     :param schema:
-    :param table:
+    :param tables:
     :return:
     """
 
     def wrapper(f):
         @functools.wraps(f)
         def inner(*args, **kwargs):
-            SyncCache.add_sync_key(schema or SyncCache.default_schema, table, f.compose_key(*args, **kwargs))
+            for table in tables:
+                SyncCache.add_sync_key(schema or SyncCache.default_schema, table, f.compose_key(*args, **kwargs))
             return f(*args, **kwargs)
 
         return inner
